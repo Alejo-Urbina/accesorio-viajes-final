cc8114befeeca38753de912ed54e75c007b51fc7
GiovyUrbina <alejoub1998@gmail.com>
2024-03-12 23:02:52 -0500

    Se agrega funcionalidad de Header y estilos

---
+++ src/componentes/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="biblioteca-header">
+      <h1 className="title">BIONVENDO A NUESTRA TIENDA DE ACCESORIOS</h1>
+      <img className="library-image" src="https://img.freepik.com/psd-premium/concepto-verano-viajes-vuelo-avion-viajes-avion-turismo-planificacion-viajes-avion-equipaje-gira-mundial-elementos-inflables-flamingo-diferentes-accesorios-verano-renderizado-3d-vacaciones_42098-709.jpg" alt="Biblioteca" />
+    </div>
+  );
+}
+
+export default Header;
