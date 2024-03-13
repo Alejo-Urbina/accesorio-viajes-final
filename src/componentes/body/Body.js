5db1d8fb968c78e13fc216d18bcbcb752a9efca0
GiovyUrbina <alejoub1998@gmail.com>
2024-03-12 23:37:25 -0500

    Se agrega componente de image

--- src/componentes/body/Body.js
+++ src/componentes/body/Body.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Body.css'; 
 import Mensajes from '../mensajes/Mensajes';
+import Images from '../images/Images';
 
 function Body() {
   return (
     <div className="library-body">
       <Mensajes />
+      <Images />
     </div>
   );
 }
